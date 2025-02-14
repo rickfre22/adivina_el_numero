6441005a4e5c3c01566aec50acf5b99db900d72a
rickfre22 <miguelgalvis@colegioguanenta.edu.co>
2025-02-14 10:11:46 -0500

    se EL README.md

---
+++ programa.py
@@ -0,0 +1,14 @@
+import random
+n=int(input("escribe un numero y intenta adivinar:"))
+cpu=random.randint(1,100)
+i=1
+while n!=cpu:
+    if n<cpu:print("el numero es mayor")
+    else:
+        print("el numero es menor")
+    n=int(input("escribe un numero otra vez: "))
+    i=i+1
+
+print("adivinaste")
+print(f"numero de intentos: {i}")
+
